d3d4bbc9fc6d12da5f317d6dab511e225925fa5c
Achin <achin@signitysolutions.in>
2019-09-13 12:42:39 +0530

    add address ui done

--- lib/src/Screens/SaveDeliveryAddress.dart
+++ lib/src/Screens/SaveDeliveryAddress.dart
@@ -9,6 +9,8 @@ class _SaveDeliveryAddressState extends State<SaveDeliveryAddress> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // set it to false
+      //body: SingleChildScrollView(child: YourBody()),
       appBar: AppBar(
           title: Text('Delivery Addresses'),
           centerTitle: true,
@@ -33,7 +35,7 @@ class _SaveDeliveryAddressState extends State<SaveDeliveryAddress> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(20, 30, 10, 10),
+            padding: EdgeInsets.fromLTRB(20, 20, 10, 10),
             child: InkWell(
               onTap: () {
                 print("Select Area click");
@@ -82,7 +84,7 @@ class _SaveDeliveryAddressState extends State<SaveDeliveryAddress> {
                     child: Align(
                       alignment: Alignment.topLeft,
                       child: new Container(
-                        color: Colors.grey,
+                        color: Colors.grey[200],
                         height: 100.0,
                         child: new TextField(
                           keyboardType: TextInputType.multiline,
@@ -92,6 +94,42 @@ class _SaveDeliveryAddressState extends State<SaveDeliveryAddress> {
                               border: InputBorder.none,
                               focusedBorder: InputBorder.none,
                               contentPadding: EdgeInsets.only( left: 5, bottom: 5, top: 5, right: 5),
+                              hintText: 'enter here'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Divider(color: Colors.grey, height: 2.0),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 10, 10),
+            child: InkWell(
+              onTap: () {
+                print("zip/postal code");
+              },
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: new Text(
+                      "Zip/Postal Code:",
+                      style: TextStyle(color: Colors.black, fontSize: 17.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: new Container(
+                        child: new TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: new InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              contentPadding: EdgeInsets.only( left: 0, bottom: 0, top: 0, right: 0),
                               hintText: 'enter here'),
                         ),
                       ),
