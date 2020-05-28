76af60f211f2df0616de040d1481e4a00bb8f3f6
apple <apple@apples-MacBook-Pro.local>
2020-05-28 16:35:31 +0530

    profile screen design completed

--- lib/src/Screens/SideMenu/ProfileScreen.dart
+++ lib/src/Screens/SideMenu/ProfileScreen.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:restroapp/src/apihandler/ApiController.dart';
 import 'package:restroapp/src/database/SharedPrefs.dart';
 import 'package:restroapp/src/models/UserResponseModel.dart';
 import 'package:restroapp/src/utils/AppColor.dart';
 import 'package:restroapp/src/utils/Utils.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileScreen extends StatefulWidget {
 
@@ -19,9 +22,11 @@ class _ProfileState extends State<ProfileScreen> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   UserModel user;
 
-  final nameController = new TextEditingController();
+  final firstNameController = new TextEditingController();
+  final lastNameController = new TextEditingController();
   final emailController = new TextEditingController();
   final phoneController = new TextEditingController();
+  File image;
 
   @override
   initState() {
@@ -34,7 +39,7 @@ class _ProfileState extends State<ProfileScreen> {
   getProfileData() async {
     user = await SharedPrefs.getUser();
     setState(() {
-      nameController.text = user.fullName;
+      firstNameController.text = user.fullName;
       emailController.text = user.email;
       phoneController.text = user.phone;
     });
@@ -48,7 +53,7 @@ class _ProfileState extends State<ProfileScreen> {
         centerTitle: true,
       ),
       body: Container(
-        color: Colors.white,
+
         /*decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage("images/offer_bg.png"), fit: BoxFit.cover),
@@ -59,70 +64,191 @@ class _ProfileState extends State<ProfileScreen> {
           child: new Form(
             key: _formKey,
             autovalidate: true,
-            child: new ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: TextField(
-                      controller: nameController,
-                      // Every single time the text changes in a
-                      // TextField, this onChanged callback is called
-                      // and it passes in the value.
-                      //
-                      // Set the text of your controller to
-                      // to the next value.
-                      //onChanged: (v) => nameController.text = v,
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: TextField(
-                      controller: emailController,
-                     // onChanged: (v) => emailController.text = v,
-                      decoration: InputDecoration(
-                        labelText: 'EmailId',
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: TextField(
-                      keyboardType: TextInputType.phone,
-                      readOnly: widget.isComingFromOtpScreen ? false : true,
-                      controller: phoneController,
-                     // onChanged: (v) => phoneController.text = v,
-                      decoration: InputDecoration(
-                        labelText: 'PhoneNumber',
-                      )),
-                ),
-                new Container(
-                  height: 20.0,
-                ),
-                new Container(
-                  height: 40.0,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new RaisedButton(
-                        child: const Text(
-                          'Save',
-                          style: TextStyle(color: Colors.white),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: setProfileImage(context),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: TextField(
+                        controller: firstNameController,
+                        decoration: InputDecoration(
+                            labelText: 'First name',
+                            floatingLabelBehavior: FloatingLabelBehavior.never
                         ),
-                        onPressed: _submitForm,
-                        color: appTheme,
+                        style: TextStyle(
+                            fontSize: 18,color: Color(0xFF495056),fontWeight: FontWeight.w500
+                        ),
                       ),
-                    ],
-                  ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: TextField(
+                        controller: lastNameController,
+                        decoration: InputDecoration(
+                            labelText: 'Last name',
+                            floatingLabelBehavior: FloatingLabelBehavior.never
+                        ),
+                        style: TextStyle(
+                            fontSize: 18,color: Color(0xFF495056),fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: Text("Private Information",style: TextStyle(
+                          fontSize: 16,color: Color(0xFF8F9396),fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: emailController,
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            floatingLabelBehavior: FloatingLabelBehavior.never
+                        ),
+                        style: TextStyle(
+                            fontSize: 18,color: Color(0xFF495056),fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: TextField(
+                        controller: phoneController,
+                        decoration: InputDecoration(
+                            labelText: 'Phone number',
+                            floatingLabelBehavior: FloatingLabelBehavior.never
+                        ),
+                        style: TextStyle(
+                            fontSize: 18,color: Color(0xFF495056),fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 35.0,left: 20,right: 20),
+                      child: ButtonTheme(
+                        height: 50,
+                        minWidth: MediaQuery.of(context).size.width,
+                        child: RaisedButton(
+                          onPressed: () => {
+
+
+                          },
+                          color: Color(0xFFF55202),
+                          shape : RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(3.0),
+                          ),
+                          child:  Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Update",style: TextStyle(color: Colors.white,fontSize: 20)),
+                              SizedBox(
+                                  width: 6
+                              ),
+                              Image.asset("images/rightArrow.png",width: 15,height: 15,),
+                            ],
+                          ),
+                        ),
+                      ) ,
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  setProfileImage(BuildContext context){
+    var img = image == null ? NetworkImage('https://via.placeholder.com/140x100') : Image.file(image).image;
+    return GestureDetector(
+      onTap: (){
+        showAlertDialog(context);
+      },
+      child: Center(
+          child: CircleAvatar(
+            backgroundColor: grayColor,
+              radius: 60,
+              backgroundImage: img
+          )
+      ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+
+    // set up the buttons
+    Widget camera = FlatButton(
+      child: Text("Camera"),
+      onPressed:  () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        getCamera();
+
+      },
+    );
+    Widget gallery = FlatButton(
+      child: Text("Gallery"),
+      onPressed:  () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        getGallery();
+
+      },
+    );
+
+    Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed:  () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Select Image"),
+      content: Text("Please select image "),
+      actions: [
+        camera,
+        gallery,
+        cancelButton
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+
+  Future getCamera() async {
+    var imageData = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      image =  imageData;
+    });
+  }
+
+  Future getGallery() async {
+    var imageData = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      image =  imageData;
+    });
   }
 
   bool isValidEmail(String input) {
@@ -137,14 +263,14 @@ class _ProfileState extends State<ProfileScreen> {
     } else {
       form.save();
       Utils.showProgressDialog(context);
-      ApiController.updateProfileRequest(nameController.text, emailController.text,
+      ApiController.updateProfileRequest(firstNameController.text, emailController.text,
           phoneController.text,widget.isComingFromOtpScreen,widget.id).then((response) {
         Utils.hideProgressDialog(context);
         if (response.success) {
           if(widget.isComingFromOtpScreen){
 
             UserModel user = UserModel();
-            user.fullName =  nameController.text.trim();
+            user.fullName =  firstNameController.text.trim();
             user.email =  emailController.text.trim();
             user.phone =  phoneController.text.trim();
             user.id = widget.id;
@@ -154,7 +280,7 @@ class _ProfileState extends State<ProfileScreen> {
             Navigator.pop(context);
 
           }else{
-            user.fullName =  nameController.text.trim();
+            user.fullName =  firstNameController.text.trim();
             user.email =  emailController.text.trim();
             user.phone =  phoneController.text.trim();
             Utils.showToast(response.message, true);
