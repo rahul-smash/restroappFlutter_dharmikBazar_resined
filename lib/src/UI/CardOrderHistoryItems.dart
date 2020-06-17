5fd902b8209435f8bd864f85442ad9e650f9b649
achin <achin@signitysolutions.in>
2020-06-17 14:57:54 +0530

    latest code

--- lib/src/UI/CardOrderHistoryItems.dart
+++ lib/src/UI/CardOrderHistoryItems.dart
@@ -98,72 +98,71 @@ class CardOrderHistoryState extends State<CardOrderHistoryItems> {
   secondRow() {
     return  Padding(
       padding: EdgeInsets.only(bottom: 15,top: 10),
-      child:  Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 12.0,top: 5.0,right: 10.0),
+            child: Row(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 12.0,top: 5.0,right: 10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text('Total Price : ',style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
-                      Text("${AppConstant.currency} ${cardOrderHistoryItems.total}",style: TextStyle(color: Colors.black,fontSize: 13))
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 12.0,top: 4.0,right: 10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Text('Status : ',style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
-                      Padding(
-                        padding: EdgeInsets.only(left: 15.0),
-                        child: Container(
-                          width: 8,
-                          height: 8,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(3.0)),
-                              color: getStatusColor(cardOrderHistoryItems.status)
-                          ),
-                        ),
-                      ),
-
-                      Padding(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Text(getStatus(cardOrderHistoryItems.status),style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
-                      )
-                    ],
-                  ),
-                ),
-
+                Text('Total Price : ',style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
+                Text("${AppConstant.currency} ${cardOrderHistoryItems.total}",style: TextStyle(color: Colors.black,fontSize: 13))
               ],
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0,top: 4.0,right: 10.0),
+            child: Row(
+              children: <Widget>[
+                Text('Status : ',style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
 
-            Padding(
-              padding: EdgeInsets.only(right: 12.0,top: 15),
-              child:  SizedBox(
-                width: 90,
-                height: 30,
-                child: FlatButton(
-                  onPressed: (){
-                    print("OrderDetailScreen");
-                  Navigator.push( context,
-                    MaterialPageRoute(
-                      builder: (context) => OrderDetailScreen(cardOrderHistoryItems),
+                Padding(
+                  padding: EdgeInsets.only(left: 15.0),
+                  child: Container(
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                        color: getStatusColor(cardOrderHistoryItems.status)
                     ),
-                  );
-                },
-                  child: Text("View Order",style: TextStyle(color: Colors.white,fontSize: 11),),
-                  color: Color(0xFFFD5401),
-                  shape:  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3.0),
                   ),
                 ),
-              ),
+
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 5.0),
+                    child: Text(getStatus(cardOrderHistoryItems.status),
+                        style: TextStyle(color: Color(0xFF39444D),fontSize: 14)),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(right: 0.0,top: 0),
+                  child:  SizedBox(
+                    width: 90,
+                    height: 30,
+                    child: FlatButton(
+                      onPressed: (){
+                        print("OrderDetailScreen");
+                        Navigator.push( context,
+                          MaterialPageRoute(
+                            builder: (context) => OrderDetailScreen(cardOrderHistoryItems),
+                          ),
+                        );
+                      },
+                      child: Text("View Order",style: TextStyle(color: Colors.white,fontSize: 11),),
+                      color: Color(0xFFFD5401),
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ]
+          ),
+
+        ],
       ),
     );
   }
