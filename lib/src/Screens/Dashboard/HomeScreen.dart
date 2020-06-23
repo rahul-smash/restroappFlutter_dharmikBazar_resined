7a0e248f59fead96aef7cdc798c5fc55857bed99
achin <achin@signitysolutions.in>
2020-06-23 09:27:31 +0530

    login same as pwa

--- lib/src/Screens/Dashboard/HomeScreen.dart
+++ lib/src/Screens/Dashboard/HomeScreen.dart
@@ -187,12 +187,12 @@ class _HomeScreenState extends State<HomeScreen> {
           items: [
             BottomNavigationBarItem(
               icon: Image.asset('images/contacticon.png', width: 24,fit: BoxFit.scaleDown,
-                  color: appTheme),
+                  color: bottomBarIconColor),
               title: Text('Contact', style: TextStyle(color: bottomBarTextColor)),
             ),
             BottomNavigationBarItem(
               icon: Image.asset('images/searchcion.png', width: 24,fit: BoxFit.scaleDown,
-                  color: appTheme),
+                  color: bottomBarIconColor),
               title: Text('Search', style: TextStyle(color: bottomBarTextColor)),
             ),
             BottomNavigationBarItem(
@@ -200,7 +200,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 title: Text(''),),
             BottomNavigationBarItem(
               icon: Image.asset('images/historyicon.png', width: 24,fit: BoxFit.scaleDown,
-                  color: appTheme),
+                  color: bottomBarIconColor),
                 title: Text('My Orders', style: TextStyle(color: bottomBarTextColor)),
                 ),
             BottomNavigationBarItem(
@@ -208,7 +208,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 showBadge: cartBadgeCount == 0 ? false : true,
                 badgeContent: Text('${cartBadgeCount}',style: TextStyle(color: Colors.white)),
                 child: Image.asset('images/carticon.png', width: 24,
-                    fit: BoxFit.scaleDown,color: appTheme),
+                    fit: BoxFit.scaleDown,color: bottomBarIconColor),
               ),
               title: Padding(
                 padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
