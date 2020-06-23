4ab3cb4c854e99aca8323d2688b007337a8a794f
apple <apple@apples-MacBook-Pro.local>
2020-06-23 10:25:13 +0530

    code commited

--- lib/src/apihandler/ApiController.dart
+++ lib/src/apihandler/ApiController.dart
@@ -179,9 +179,9 @@ class ApiController {
   }
 
   static Future<CategoryResponse> getCategoriesApiRequest(String storeId) async {
-    var url = ApiConstants.baseUrl.replaceAll("storeId", storeId) +
+     var url = ApiConstants.baseUrl.replaceAll("storeId", storeId) +
         ApiConstants.getCategories;
-
+    print("catttttt  $url");
     Response response = await Dio().get(url,options: new Options(responseType: ResponseType.plain));
     //print(response);
     CategoryResponse categoryResponse = CategoryResponse.fromJson(json.decode(response.data));
