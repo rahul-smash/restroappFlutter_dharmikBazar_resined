03ffcf3380b35974aa16a652f9e70daee6b1fdfa
karan <karan@signitysolutions.in>
2020-09-04 11:23:26 +0530

    Home Screen Issue Fixed

--- lib/src/UI/CategoryView.dart
+++ lib/src/UI/CategoryView.dart
@@ -18,72 +18,71 @@ class CategoryView extends StatelessWidget {
       this.categoryModel, this.store, this.isComingFromBaner, this.index);
 
   Widget build(BuildContext context) {
-    return Container(
-      width: Utils.getDeviceWidth(context),
-      margin: EdgeInsets.fromLTRB(10, 0, 10, 15),
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () async {
-              if (checkIfStoreClosed()) {
-                DialogUtils.displayCommonDialog(
-                    context, store.storeName, store.storeMsg);
-              } else {
-                if (categoryModel != null &&
-                    categoryModel.subCategory.isNotEmpty) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return SubCategoryProductScreen(
-                          categoryModel, isComingFromBaner, index);
-                    }),
-                  );
-                  Map<String, dynamic> attributeMap =
-                      new Map<String, dynamic>();
-                  attributeMap["ScreenName"] = "${categoryModel.title}";
-                  Utils.sendAnalyticsEvent("Clicked category", attributeMap);
-                } else {
-                  if (categoryModel != null &&
-                      categoryModel.subCategory != null) {
-                    if (categoryModel.subCategory.isEmpty) {
-                      Utils.showToast("No data found!", false);
-                    }
-                  }
+    return InkWell(
+        onTap: () async {
+          if (checkIfStoreClosed()) {
+            DialogUtils.displayCommonDialog(
+                context, store.storeName, store.storeMsg);
+          } else {
+            if (categoryModel != null && categoryModel.subCategory.isNotEmpty) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return SubCategoryProductScreen(
+                      categoryModel, isComingFromBaner, index);
+                }),
+              );
+              Map<String, dynamic> attributeMap = new Map<String, dynamic>();
+              attributeMap["ScreenName"] = "${categoryModel.title}";
+              Utils.sendAnalyticsEvent("Clicked category", attributeMap);
+            } else {
+              if (categoryModel != null && categoryModel.subCategory != null) {
+                if (categoryModel.subCategory.isEmpty) {
+                  Utils.showToast("No data found!", false);
                 }
               }
-            },
-            child: Container(
-              margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
-              width: Utils.getDeviceWidth(context),
-              height: 100.0,
-              child: CachedNetworkImage(
-                  imageUrl: "${categoryModel.image300200}",
+            }
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(shape: BoxShape.rectangle,color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+          width: Utils.getDeviceWidth(context),
+          margin: EdgeInsets.fromLTRB(10, 0, 10, 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
                   width: Utils.getDeviceWidth(context),
-                  height: 100.0,
-                  fit: BoxFit.cover
-                  //placeholder: (context, url) => CircularProgressIndicator(),
-                  //errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-              child: Center(
-                child: Text(
-                    categoryModel.title,
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: new TextStyle(color: Colors.black, fontSize: 14.0)),
+//                  height: 80.0,
+                  child: CachedNetworkImage(
+                      imageUrl: "${categoryModel.image300200}",
+                      width: Utils.getDeviceWidth(context),
+//                      height: 80.0,
+                      fit: BoxFit.cover
+                      //placeholder: (context, url) => CircularProgressIndicator(),
+                      //errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                  child: Center(
+                    child: Text(categoryModel.title,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style:
+                            new TextStyle(color: Colors.black, fontSize: 14.0)),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 
   bool checkIfStoreClosed() {
