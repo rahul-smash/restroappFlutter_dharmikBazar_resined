2c33f627425544f0bfa2cf3d18d6fc13ce6e3cb2
karan <karan@signitysolutions.in>
2020-10-07 12:40:15 +0530

    Features

--- lib/src/Screens/Notification/NotificationScreen.dart
+++ lib/src/Screens/Notification/NotificationScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_pull_to_refresh/flutter_pull_to_refresh.dart';
 import 'package:restroapp/src/apihandler/ApiController.dart';
 import 'package:restroapp/src/models/NotificationResponseModel.dart';
 import 'package:restroapp/src/utils/AppColor.dart';
@@ -17,48 +18,49 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   void initState() {
     super.initState();
-    ApiController.getAllNotifications().then((value) {
-      setState(() {
-        isLoadingApi = false;
-        if (value != null && value.success) {
-          resposneModel = value;
-        }
-      });
-    });
+    getNotificationApi();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Text('Notifications'),
-        centerTitle: true,
-      ),
-      backgroundColor: grayLightColor,
-      body: isLoadingApi
-          ? Container(child: Center(child: CircularProgressIndicator()))
-          : resposneModel != null
+        appBar: AppBar(
+          title: new Text('Notifications'),
+          centerTitle: true,
+        ),
+        backgroundColor: grayLightColor,
+        body: PullToRefreshView(
+          onRefresh: () {
+            return getNotificationApi();
+          },
+          child: isLoadingApi
+              ? Container( color: grayLightColor,
+              child: Center(child: CircularProgressIndicator()))
+              : resposneModel != null
               ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Flexible(
-                        child: Container(
-                            margin: EdgeInsets.fromLTRB(15, 15, 15, 10),
-                            child: ListView.builder(
-                              itemBuilder: (context, index) => _makeCard(index),
-                              itemCount: resposneModel.data.length,
-                              scrollDirection: Axis.vertical,
-                              shrinkWrap: false,
-                            )))
-                  ],
-                )
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                  child: Container(
+                    color: grayLightColor,
+                      padding: EdgeInsets.fromLTRB(15, 15, 15, 10),
+                      child: ListView.builder(
+                        itemBuilder: (context, index) =>
+                            _makeCard(index),
+                        itemCount: resposneModel.data.length,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: false,
+                      )))
+            ],
+          )
               : Utils.getEmptyView2("No Notifications"),
-    );
+        ));
   }
 
   _makeCard(int index) {
-    var cellbgColor = Colors.white/*index % 2 == 0 ? appTheme.withOpacity(.5) : appThemeLight*/;
+    var cellbgColor = Colors
+        .white /*index % 2 == 0 ? appTheme.withOpacity(.5) : appThemeLight*/;
     return InkWell(
       child: Container(
         decoration: BoxDecoration(
@@ -93,14 +95,19 @@ class _NotificationScreenState extends State<NotificationScreen> {
                       children: <Widget>[
                         Padding(
                           padding: EdgeInsets.only(left: 5, top: 10),
-                          child: Text(Utils.convertNotificationDateTime(resposneModel.data[index].created),
+                          child: Text(
+                              Utils.convertNotificationDateTime(
+                                  resposneModel.data[index].created),
                               style:
-                                  TextStyle(fontSize: 14, color: Colors.black)),
+                              TextStyle(fontSize: 14, color: Colors.black)),
                         ),
                         Padding(
                             padding:
-                                EdgeInsets.only(left: 5, right: 5, top: 10),
-                            child: Text(Utils.convertNotificationDateTime(resposneModel.data[index].created,onlyTime: true),
+                            EdgeInsets.only(left: 5, right: 5, top: 10),
+                            child: Text(
+                                Utils.convertNotificationDateTime(
+                                    resposneModel.data[index].created,
+                                    onlyTime: true),
                                 style: TextStyle(
                                     fontSize: 14, color: Colors.black))),
                       ],
@@ -112,16 +119,28 @@ class _NotificationScreenState extends State<NotificationScreen> {
           ],
         ),
       ),
-      onTap: (){
-       if(resposneModel.data[index].type.toLowerCase().compareTo('order')==0){
-         Navigator.push(
-           context,
-           MaterialPageRoute(builder: (context) {
-             return NotificationScreen();
-           }),
-         );
-       }
+      onTap: () {
+        if (resposneModel.data[index].type.toLowerCase().compareTo('order') ==
+            0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return NotificationScreen();
+            }),
+          );
+        }
       },
     );
+  }
+
+  Future<Null> getNotificationApi() {
+    return ApiController.getAllNotifications().then((value) {
+      setState(() {
+        isLoadingApi = false;
+        if (value != null && value.success) {
+          resposneModel = value;
+        }
+      });
+    });
   }
 }
