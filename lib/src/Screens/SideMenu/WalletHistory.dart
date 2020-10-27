edcba3a5ca01753b32504f9b0c2dbe80e4daeb82
achin <achin@signitysolutions.in>
2020-10-27 14:37:24 +0530

    wallet listing UI completed

--- lib/src/Screens/SideMenu/WalletHistory.dart
+++ lib/src/Screens/SideMenu/WalletHistory.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:restroapp/src/apihandler/ApiController.dart';
 import 'package:restroapp/src/models/WalleModel.dart';
 import 'package:restroapp/src/utils/AppColor.dart';
@@ -31,7 +34,7 @@ class _WalletHistoryScreenState extends State<WalletHistoryScreen> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: appTheme,
         title: Text("Wallet Balance",style:TextStyle(color: Colors.white,),),
@@ -43,6 +46,8 @@ class _WalletHistoryScreenState extends State<WalletHistoryScreen> {
       ),
       body: walleModel == null
           ? Utils.showIndicator()
+          : walleModel.data.walletHistory.isEmpty
+          ? Container(child: showEmptyWidget(),)
           : SafeArea(
         child: Container(
           child: Stack(
@@ -195,160 +200,80 @@ class _WalletHistoryScreenState extends State<WalletHistoryScreen> {
       ],
     );
   }
-}
 
-class WalletViewWidget extends StatefulWidget {
-
-  @override
-  WalletViewWidgetState createState() => WalletViewWidgetState();
-}
-
-class WalletViewWidgetState extends State<WalletViewWidget> {
-
-  WalleModel walleModel;
-
-  @override
-  void initState() {
-    super.initState();
-    ApiController.getUserWallet().then((response){
-      setState(() {
-        this.walleModel = response;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: appTheme,
-        title: Text("Wallet Balance",style:TextStyle(color: Colors.white,),),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
-      body: walleModel == null
-          ? Utils.showIndicator()
-          : Stack(
-        children: [
-
-          Container(
-              margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Wallet Balance",style: TextStyle(color: blue2,fontSize: 16),),
-                  Row(
-                    children: [
-                      Text("${AppConstant.currency}",
-                          style: TextStyle(color: Colors.white)),
-                      Text("${walleModel.data.userWallet}",
-                          style: TextStyle(color: Colors.white,fontSize: 24)),
-                    ],
-                  )
-                ],
-              )
-          ),
-
-          SizedBox.expand(
-            child: DraggableScrollableSheet(
-              //initialChildSize: 0.1,
-              //minChildSize: 0.1,
-              builder: (BuildContext context, ScrollController scrollController) {
-
-                return Container(
-                  color: Colors.blue[100],
-                  child: ListView.builder(
-                    controller: scrollController,
-                    itemCount: 1,
-                    itemBuilder: (BuildContext context, int index) {
-
-
-                      return ListTile(title: Text('Item $index'));
-                    },
-                  ),
-                );
-
-              },
-            ),
-          ),
-
-        ],
-      ),
-    );
-  }
-
-  Widget showWalletView(WalletHistory walletHistory) {
-
-    return Row(
+  Widget showEmptyWidget() {
+    print("-------showEmptyWidget-----------");
+    return Stack(
       children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-          //child: Icon(Icons.add_circle_outline),
-          child: Container(
-            height: 45,width: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              border: Border.all(
-                width: 0.0,
-                color: walletHistory.refund_type=="order_refund"?red1:green1,
-              ),
-              color: walletHistory.refund_type=="order_refund"?red1:green1,
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(6),
-              child: Image.asset(walletHistory.refund_type == "order_refund"
-                  ? "images/orderrefund.png"
-                  : "images/cashbackicon.png",height: 20,width: 20,fit: BoxFit.fill,),
-            ),
-          ),
-          /*child: Image.asset(walletHistory.refund_type == "order_refund"
-              ? "images/orderrefund.png"
-              : "images/cashbackicon.png"),*/
-        ),
-        Expanded(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('${walletHistory.label}',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal)),
-                SizedBox(height: 5),
-                Text("#${walletHistory.displayOrderId}",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                        color: Colors.grey[500], fontSize: 15)),
-              ]
-          ),
-        ),
         Container(
-          margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Text('${walletHistory.refund_type == "order_refund" ? " - " : " + "}${AppConstant.currency}${walletHistory.refund}',
-                    style: TextStyle(
-                        color: walletHistory.refund_type=="order_refund"?Colors.black:green2,
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal)),
-                SizedBox(height: 5),
-                Text("${Utils.convertWalletDate(walletHistory.dateTime.toString())}",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                        color: Colors.grey[500], fontSize: 15)),
-              ]
+            height: Utils.getDeviceHeight(context)/2.2,
+            width: Utils.getDeviceWidth(context),
+            //color: Colors.white,
+            child: Container(
+              child: ClipPath(
+                clipper: ClippingClass(),
+                child: Container(
+                  color: Colors.grey[300],
+                  //child: Text("Textview"),
+                ),
+              ),
+            )
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            //color: Colors.grey,
+            child: Center(
+              child: Column(
+                children: [
+                  Text("Your Wallet is Empty",style:
+                  TextStyle(color: blue1,fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  Text("Look like there are no credit"),
+                  Text("in your wallet at that moment"),
+                  Text("kindly purchase moreto continue"),
+                  Text("to continue"),
+                  SizedBox(height: 20,),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(40, 0, 0, (Utils.getDeviceHeight(context)/3.5)),
+          child: Align(
+            alignment: Alignment.center,
+            child: Image.asset("images/emptywalletbalancegreaphics.png",
+              width: 240,height: 240,),
           ),
         ),
       ],
     );
   }
-
 }
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 30);
+
+    path.quadraticBezierTo(size.width/4, size.height, size.width/2, size.height);
+
+    path.quadraticBezierTo(size.width -(size.width/4), size.height,
+        size.width, size.height- 30);
+
+    path.lineTo(size.width, 0.0);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
+
