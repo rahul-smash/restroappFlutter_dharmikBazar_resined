f36288d44a5c7e9c546af4ef095d53dc1ac2b0d3
achin <achin@signitysolutions.in>
2020-12-29 11:14:26 +0530

    Thank you screen

--- lib/src/Screens/LoginSignUp/OtpScreen.dart
+++ lib/src/Screens/LoginSignUp/OtpScreen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:sms_autofill/sms_autofill.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:restroapp/src/Screens/Address/DeliveryAddressList.dart';
