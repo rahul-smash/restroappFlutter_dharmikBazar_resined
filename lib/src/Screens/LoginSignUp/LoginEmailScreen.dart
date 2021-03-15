44b4e44b11bbb0b0468656f3abdac59e579471a8
karan <karan@signitysolutions.in>
2021-03-15 15:34:32 +0530

    Updated Code with versions

--- lib/src/Screens/LoginSignUp/LoginEmailScreen.dart
+++ lib/src/Screens/LoginSignUp/LoginEmailScreen.dart
@@ -82,7 +82,7 @@ class _LoginEmailScreenState extends State<LoginEmailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteColor,
-      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         centerTitle: true,
         title: new Text('Login',style: new TextStyle(
