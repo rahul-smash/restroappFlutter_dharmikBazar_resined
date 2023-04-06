29360e8c8679b3c3d782e0a6bf4fb7fe8da0a267
diksha.pathania <diksha.pathania@signitysolutions.com>
2023-04-06 11:54:55 +0530

    Resolved bugs.

--- lib/main.dart
+++ lib/main.dart
@@ -80,10 +80,8 @@ Future<void> main() async {
   SharedPrefs.storeSharedValue(AppConstant.isAdminLogin, "$isAdminLogin");
 
   PackageInfo packageInfo = await Utils.getAppVersionDetails(storeData);
- SystemUiOverlayStyle.light.copyWith(
-   statusBarIconBrightness: Brightness.dark,
-   statusBarColor: whiteColor
- );
+  SystemUiOverlayStyle.light.copyWith(
+      statusBarIconBrightness: Brightness.dark, statusBarColor: whiteColor);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   Utils.getDeviceInfo(storeData);
   // To turn off landscape mode
